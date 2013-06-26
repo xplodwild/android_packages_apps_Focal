686c9235b9b9a94e0939f74c6d1d77c9e0daa6df
xplodwild <xplodgui@gmail.com>
2013-06-26 23:55:00 +0200

    Add contrast widget

--- src/org/cyanogenmod/nemesis/CameraCapabilities.java
+++ src/org/cyanogenmod/nemesis/CameraCapabilities.java
@@ -53,6 +53,7 @@ public class CameraCapabilities {
         mWidgets.add(new HdrWidget(cam, context));
         mWidgets.add(new EffectWidget(cam, context));
         mWidgets.add(new SkinToneWidget(cam, context));
+        mWidgets.add(new ContrastWidget(cam, context));
         mWidgets.add(new BurstModeWidget(context));
         mWidgets.add(new VideoFrWidget(cam, context));
         mWidgets.add(new SettingsWidget(cam, context));
