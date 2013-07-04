62f1c430d4a7a805cc15dbee70631511f087c4e4
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-04 17:57:29 +0530

    build all the awesome tools

--- Android.mk
+++ Android.mk
@@ -6,6 +6,28 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := Nemesis
 
+LOCAL_REQUIRED_MODULES := \
+	libexiv2 \
+	libglib-2.0 \
+	libgmodule-2.0 \
+	libgobject-2.0 \
+	libgthread-2.0 \
+	libjpeg \
+	libpano13 \
+	libtiffdecoder \
+	libvigraimpex \
+	libhugin \
+	libxml2 \
+	libiconv \
+	libpng_static \
+	autooptimiser \
+	autopano \
+	celeste \
+	nona \
+	ptclean \
+	enblend \
+	enfuse
+
 include $(BUILD_PACKAGE)
 
 -include $(call all-makefiles-under, $(ANDROID_BUILD_TOP)/external/Nemesis)
