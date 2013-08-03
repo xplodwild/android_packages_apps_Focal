a0b40a733052f856c4b69c3d412d0cbb9843b7dc
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-08-03 20:10:40 +0900

    Don't build if device use the camera stub
    Change-Id: Ibdacb7405364b57ed8bfbf96fbbee2d12c3d1b7a

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(USE_CAMERA_STUB),true)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -44,3 +46,5 @@ LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := metadata-extractor:libs/metadata-extract
 include $(BUILD_MULTI_PREBUILT)
 
 include $(call all-makefiles-under, $(ANDROID_BUILD_TOP)/external/Focal)
+
+endif # ifneq ($(USE_CAMERA_STUB),true)
